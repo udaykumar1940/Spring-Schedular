993f8ae936877c34a332d063801b1cb1cb0c85ce
UdayKatta <udaykumar1940@gmail.com>
2022-01-02 11:44:47 +0530

    commit 15

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -16,12 +16,12 @@ import org.springframework.scheduling.annotation.Scheduled;
 @ComponentScan(basePackages = "com.example")
 public class Application {
 
-	//This is main method,testing Squash in git 4
+	//This is main method,testing Squash in git 5
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
 	
-	//Every Hour schedulerMethod,testing Squash in git 4
+	//Every Hour schedulerMethod,testing Squash in git commit 17
 	@Scheduled(cron = "0 0 0/1 1/1 * ?")
 	public void schedulerMethod() {
 		System.out.println("Every Hour schedulerMethod=>" + new Date());
